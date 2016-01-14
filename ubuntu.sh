a28fdbca99886f398a8ddb3e30e76033596e5731
Jiayu Liu <jimexist@gmail.com>
2016-01-14 21:57:20 +0800

    add pyenv

--- ubuntu.sh
+++ ubuntu.sh
@@ -3,6 +3,11 @@
 # install git
 sudo apt-get install git
 
+# pyenv
+curl -L
+https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer
+| bash
+
 # install nvm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.30.1/install.sh |
 bash
