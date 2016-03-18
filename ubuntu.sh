0170b6a7b28fd24a78e9e8f82b97830a3d4657af
Jiayu Liu <Jimexist@users.noreply.github.com>
2016-03-18 11:58:21 +0800

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -6,9 +6,13 @@ sudo apt-get install -y git
 # zsh
 sudo apt-get install -y zsh
 
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 # pyenv
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev
 curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+pyenv install 2.7.11
 
 # install nvm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
