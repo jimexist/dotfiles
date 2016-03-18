afe1e5320df27c2508749e7ed932c33bdeebcd80
Jiayu Liu <Jimexist@users.noreply.github.com>
2016-03-18 11:53:14 +0800

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,23 +1,21 @@
 #!/bin/sh
 
 # install git
-sudo apt-get install git
+sudo apt-get install -y git
 
 # zsh
-sudo apt-get install zsh
+sudo apt-get install -y zsh
 
 # pyenv
-curl -L
-https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer
-| bash
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev
+curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
 
 # install nvm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.30.1/install.sh |
-bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
 nvm install node
 
 # oracle java 8
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get install oracle-java8-installer
-sudo apt-get install oracle-java8-set-default
+sudo apt-get install -y oracle-java8-installer
+sudo apt-get install -y oracle-java8-set-default
