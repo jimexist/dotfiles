85833a59a86663ba182378f818307ea3c27c9fc9
Jiayu Liu <jimexist@gmail.com>
2016-03-21 11:36:12 +0800

    merge

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,25 +1,23 @@
 #!/bin/sh
-
-# install essentials
-sudo apt-get install -y git zsh wget
-
-# oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# the fuck
-wget -O - https://raw.githubusercontent.com/nvbn/thefuck/master/install.sh | sh - && $0
-
-# pyenv
-sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev
-curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
-pyenv install 2.7.11
-
-# install nvm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
-nvm install node
-
-# oracle java 8
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get install -y oracle-java8-installer
-sudo apt-get install -y oracle-java8-set-default
+
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+
+# install git
+sudo apt-get install -y \
+    build-essential \
+    git \
+    oracle-java8-installer \
+    oracle-java8-set-default \
+    maven
+
+# pyenv
+curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+pyenv install 2.7.11
+pyenv global 2.7.11
+
+# install nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.30.1/install.sh | bash
+nvm install node
+
