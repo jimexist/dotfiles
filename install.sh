8ba70771f6990a5eb3beb922901694c5a685fa04
Jiayu Liu <jiayu@hey.com>
2022-11-13 17:42:13 +0800

    rust install

--- install.sh
+++ install.sh
@@ -11,9 +11,18 @@ ln -sf "${BASEDIR}/psqlrc" ~/.psqlrc
 # install or update starship
 curl -sS https://starship.rs/install.sh | sh -s -- -y
 
+init_codespaces() {
+  # rust
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+  # python
+  python -m pip install -U pip
+}
+
 if [[ "$(uname -n)" == "codespaces-"* ]]; then
   ln -sf "${BASEDIR}/codespaces/bashrc" ~/.bashrc
   ln -sf "${BASEDIR}/codespaces/gitconfig" ~/.gitconfig
+
+  init_codespaces
 else
   ln -sf "${BASEDIR}/zshrc" ~/.zshrc
   ln -sf "${BASEDIR}/gitconfig" ~/.gitconfig
