d70aa21a88f0af351cffbd40801d944a7fb9d63a
Jiayu Liu <jiayu@hey.com>
2022-11-13 17:34:42 +0800

    bash rc
    bashrc

--- install.sh
+++ install.sh
@@ -8,23 +8,13 @@ ln -sf "${BASEDIR}/vim" ~/.vim
 ln -sf "${BASEDIR}/gitignore" ~/.gitignore
 ln -sf "${BASEDIR}/psqlrc" ~/.psqlrc
 
-# install oh my zsh
-# if ! command -v omz &> /dev/null
-# then
-#   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-# else
-#   omz update
-# fi
-
 # install or update starship
 curl -sS https://starship.rs/install.sh | sh -s -- -y
 
-if [[ "$(uname -n)" == *"codespaces-"* ]]; then
-  ln -sf "${BASEDIR}/codespaces/zshrc" ~/.zshrc
+if [[ "$(uname -n)" == "codespaces-"* ]]; then
+  ln -sf "${BASEDIR}/codespaces/bashrc" ~/.bashrc
   ln -sf "${BASEDIR}/codespaces/gitconfig" ~/.gitconfig
 else
   ln -sf "${BASEDIR}/zshrc" ~/.zshrc
   ln -sf "${BASEDIR}/gitconfig" ~/.gitconfig
 fi
-
-sudo chsh -s $(which zsh)
